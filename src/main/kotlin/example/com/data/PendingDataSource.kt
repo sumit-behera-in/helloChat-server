2b3ba4d6574629d7f8facbef80005b02893c8a0d
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-07-29 01:48:57 +0530

    add and send PendingDataSource added

--- src/main/kotlin/example/com/data/PendingDataSource.kt
+++ src/main/kotlin/example/com/data/PendingDataSource.kt
@@ -1,19 +1,43 @@
 package example.com.data
 
 import com.mongodb.client.MongoDatabase
+import io.ktor.websocket.Frame
 import io.ktor.websocket.WebSocketSession
+import kotlinx.coroutines.coroutineScope
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.bson.Document
 
 class PendingDataSource(
     private val db: MongoDatabase,
 ) {
 
     suspend fun addPendingMessage(message: Message) {
-        // val collection = db.getCollection(message.receiver)
-
+        coroutineScope {
+            val messagesCollection = db.getCollection(message.receiver)
+            try {
+                messagesCollection.insertOne(message.toDocument())
+            } catch (e: Exception) {
+                // Handle exception
+                println("Error adding pending message: $e")
+            }
+        }
     }
 
     suspend fun sendPendingMessages(receiver: String, sockets: WebSocketSession?) {
-
+        sockets?.let {
+            val messagesCollection = db.getCollection(receiver)
+            val messages = messagesCollection.find().toList()
+            messages.forEach { doc: Document ->
+                try {
+                    it.send(Frame.Text(Json.encodeToString(doc)))
+                    messagesCollection.deleteOne(doc)
+                } catch (e: Exception) {
+                    // Handle exception
+                    println("Error sending pending message: $e")
+                }
+            }
+        }
     }
 
 }
