f15d2569ad7dc982511345f3334e5c9439dd2f42
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-07-29 01:16:30 +0530

    koin added

--- src/main/kotlin/example/com/Application.kt
+++ src/main/kotlin/example/com/Application.kt
@@ -1,16 +1,22 @@
 package example.com
 
+import example.com.di.MainModule
 import example.com.plugins.*
 import io.ktor.server.application.*
+import org.koin.ktor.plugin.Koin
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+
+    install(Koin) {
+       modules(MainModule)
+    }
+
     configureSockets()
     configureSerialization()
-    configureDatabases()
     configureSecurity()
     configureRouting()
 }
