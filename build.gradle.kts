a565c9931307a288972d6300f986fa34a5e36519
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-07-29 01:16:39 +0530

    koin added

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 val kotlin_version: String by project
 val logback_version: String by project
 val mongo_version: String by project
+val koin_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.0"
@@ -36,4 +37,9 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // Koin core features
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
 }
